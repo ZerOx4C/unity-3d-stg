91f8559b363071163052146475124553261a381a
ZerOx4C <zerox4c@gmail.com>
2025-01-12 03:10:48 +0900

    機体が申し訳程度に進むようにした

--- Assets/Runtime/AircraftController.cs
+++ Assets/Runtime/AircraftController.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class AircraftController : MonoBehaviour
 {
     private GameObject _model;
+    private Rigidbody _rigidbody;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _rigidbody.linearVelocity = transform.forward;
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.linearVelocity += Physics.gravity.magnitude * Time.fixedDeltaTime * Vector3.up;
+    }
 
     public async UniTask SetModel(GameObject prefab, CancellationToken cancellation)
     {
